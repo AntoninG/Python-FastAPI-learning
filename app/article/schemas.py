e100487a0df895143f8e3667c70e589a45ae0560
AntoninG <antonin.guilet-dupont@laposte.net>
2022-07-12 18:34:17 +0200

    CRUD articles with schemas

--- app/article/schemas.py
+++ app/article/schemas.py
@@ -4,3 +4,10 @@ from pydantic import BaseModel
 class Article(BaseModel):
     title: str
     content: str
+
+
+class ArticleResource(Article):
+    id: int
+
+    class Config:
+        orm_mode = True
